96bd4421af9b4939c829ae948475e2ef1e1a37bc
LeWade <41769659+LeWade@users.noreply.github.com>
2019-04-08 20:52:16 +0400

    страница логирования + css
    страница логирования + css
    js по данной странице - в работе

---
+++ auth/authError.js
@@ -0,0 +1,8 @@
+// function checkString () {
+//     var logIn = document.getElementById ("inLogin");
+//      //var pasIn = document.getElementById ('in');
+
+//      if (logIn === null) {
+//          alert ("Не заполнен логин!");
+//     console.log(logIn);
+// }
